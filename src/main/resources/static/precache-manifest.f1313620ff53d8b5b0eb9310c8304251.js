963adf920417349e67ffdb93848eebc779a1c40d
Nader <na511dr@hotmail.com>
2020-06-12 01:58:36 +0300

    fix bugs in static views

---
+++ src/main/resources/static/precache-manifest.f1313620ff53d8b5b0eb9310c8304251.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48eb9ae00db9d0c2db742ed1f52a5765",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d198f9a8e4cddf38634c",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b18786f9ab29847d7812",
+    "url": "/static/css/main.3ed61038.chunk.css"
+  },
+  {
+    "revision": "d198f9a8e4cddf38634c",
+    "url": "/static/js/2.030884f2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.030884f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b18786f9ab29847d7812",
+    "url": "/static/js/main.244b9d33.chunk.js"
+  },
+  {
+    "revision": "773a84eb9cfeab52d79f",
+    "url": "/static/js/runtime-main.78d46128.js"
+  }
+]);
